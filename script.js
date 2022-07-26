638b2c243bb0eacde68c01c9732444ed061d92eb
habwa <89010758+habwa@users.noreply.github.com>
2022-07-26 11:00:41 +0200

    this is from sc...

---
+++ script.js
@@ -0,0 +1,8 @@
+ function myFunction() {
+     document.getElementById("demo").innerHTML= 'hello there'
+ }
+ var a,b,c;
+ a=5;
+ b=6;
+ c=a+b;
+ document.getElementById('demo1').innerText=`the value of c is equal to${c}.`;
